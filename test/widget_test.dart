a4158d5f37e7d42ba3a46b5eccb667b96d3857e2
stafel <rafael.stauffer@hotmail.de>
2024-02-16 16:19:27 +0100

    test: drawer opening

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,23 +10,23 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:frogger/main.dart';
 
-/*
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Drawer opens', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+
+    // check our title
+    expect(find.text('Frogger'), findsOneWidget);
+    expect(find.text('This text does not exist'), findsNothing);
+    expect(find.text('Login'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byType(IconButton));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // check if drawer opened
+    expect(find.text('Login'), findsOneWidget);
   });
 }
-*/
