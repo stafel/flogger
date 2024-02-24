0d342e25ec46e54272fb185d0ea5522d3c8407f5
stafel <rafael.stauffer@hotmail.de>
2024-02-24 20:06:02 +0100

    test: fix blogcard test with testscaffold

--- test/blogcard_test.dart
+++ test/blogcard_test.dart
@@ -4,69 +4,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frogger/models/author.dart';
+import 'package:frogger/models/blog.dart';
 
 import 'package:frogger/models/blogentry.dart';
 import 'package:frogger/views/blogcard.dart';
+import 'package:provider/provider.dart';
 
-// TODO: fix tests
-/*
+// Contains all required widgets for the blog entry to work
+class TestScaffold extends StatelessWidget {
+  final Widget child;
+  final List<BlogEntry> initialEntries;
+
+  TestScaffold({
+    super.key,
+    required this.child,
+    required this.initialEntries
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Blog>(
+              create: (_) {
+                final myBlog = Blog();
+                initialEntries.forEach((element) { myBlog.add(element); });
+                return myBlog;
+                },
+              child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: child
+      ));
+  }
+}
+
+
 void main() {
-  testWidgets('Show number of likes one or more', (WidgetTester tester) async {
-    var blogEntryOneLike = BlogEntry(id: "test", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: false, totalLikes: 1, author: User(id: "null", username: "username"));
+  final testEntryOneLike = BlogEntry(id: "test1", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: false, totalLikes: 1, author: User(id: "null", username: "username"));
+  final testEntryZeroLike = BlogEntry(id: "test0", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: false, totalLikes: 0, author: User(id: "null", username: "username"));
+  final testEntryLikedByMe = BlogEntry(id: "test0", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: true, totalLikes: 1, author: User(id: "null", username: "username"));
 
+  testWidgets('Show number of likes one or more', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     // Wrap in directionality to prevent no directionality error because of the missing framework
     // Because the like widget consumes an api and a blogentry it will not work
     await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: BlogCard(blogEntry: blogEntryOneLike)
-      ));
+      TestScaffold(initialEntries: [testEntryOneLike], child: BlogCard(blogId: testEntryOneLike.id)));
 
     // total likes should be displayed
     expect(find.text('1'), findsOneWidget);
   });
 
   testWidgets('Show no number of likes one zero', (WidgetTester tester) async {
-    var blogEntryOneLike = BlogEntry(id: "test", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: false, totalLikes: 0, author: User(id: "null", username: "username"));
-
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: BlogCard(blogEntry: blogEntryOneLike)
-      ));
+      TestScaffold(initialEntries: [testEntryZeroLike], child: BlogCard(blogId: testEntryZeroLike.id)));
 
     // total likes should be displayed
     expect(find.text('0'), findsNothing);
   });
 
   testWidgets('Show not liked by me heart', (WidgetTester tester) async {
-    var blogEntryOneLike = BlogEntry(id: "test", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: false, totalLikes: 0, author: User(id: "null", username: "username"));
-
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: BlogCard(blogEntry: blogEntryOneLike)
-      ));
+      TestScaffold(initialEntries: [testEntryOneLike], child: BlogCard(blogId: testEntryOneLike.id)));
 
     // total likes should be displayed
     expect(find.byIcon(Icons.favorite_border), findsOneWidget);
   });
 
   testWidgets('Show liked by me heart', (WidgetTester tester) async {
-    var blogEntryOneLike = BlogEntry(id: "test", title: "test", content: "shocking stuff", creationDate: "01.01.2021", liked: true, totalLikes: 0, author: User(id: "null", username: "username"));
-
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      Directionality(
-        textDirection: TextDirection.ltr,
-        child: BlogCard(blogEntry: blogEntryOneLike)
-      ));
+      TestScaffold(initialEntries: [testEntryLikedByMe], child: BlogCard(blogId: testEntryLikedByMe.id)));
 
     // total likes should be displayed
     expect(find.byIcon(Icons.favorite), findsOneWidget);
   });
 }
-*/
