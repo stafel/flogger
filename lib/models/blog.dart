9659be9cc4fc4ee77792e2195df74c2493f0c543
stafel <rafael.stauffer@hotmail.de>
2024-02-25 12:41:16 +0100

    feat: create and update blog func

--- lib/models/blog.dart
+++ lib/models/blog.dart
@@ -1,8 +1,10 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:frogger/models/author.dart';
 import 'package:frogger/models/blogentry.dart';
 import 'package:frogger/services/blogapi.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 enum BlogState {
@@ -80,6 +82,29 @@ class Blog extends ChangeNotifier {
 
   unlikeBlog(String blogId) {
     api?.unlikeBlog(blogId);
+    notifyListeners();
+  }
+
+  createBlogSimple(String title, String content) {
+    String formattedDate = DateFormat('dd.MM.yyyy').format(DateTime.now());
+
+    User user = User(id: api?.userId ?? "testid", username: api?.userName ?? "anon");
+
+    final newEntry = BlogEntry(id: "", title: title, content: content, creationDate: formattedDate, liked: false, totalLikes: 0, author: user);
+
+    createBlog(newEntry);
+  }
+
+  createBlog(BlogEntry entry) {
+    api?.updateBlogEntry(entry);
+    add(entry);
+  }
+
+  updateBlog(BlogEntry entry) {
+    api?.updateBlogEntry(entry);
+    final index = _items.indexWhere((element) => element.id == entry.id);
+    _items.removeAt(index);
+    _items.insert(index, entry);
     notifyListeners();
   }
 }
